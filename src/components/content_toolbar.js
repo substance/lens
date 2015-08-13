c8cf08ae4a209e0de02a3d6ac3a30ad123cf41a0
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-08-13 13:16:43 +0200

    Use OpenModalTool component.

--- src/components/content_toolbar.js
+++ src/components/content_toolbar.js
@@ -5,14 +5,14 @@ var OO = Substance.OO;
 var Component = Substance.Component;
 var $$ = Component.$$;
 
-var ToolComponent = require("substance/ui/tools/tool_component");
-var TextToolComponent = require("substance/ui/tools/text_tool_component");
-var TableToolComponent = require("substance/ui/tools/table_tool_component");
+var Tool = require("substance/ui/tools/tool_component");
+var TextTool = require("substance/ui/tools/text_tool_component");
+var TableTool = require("substance/ui/tools/table_tool_component");
+var OpenModalTool = require("substance/ui/tools/openmodal_tool_component");
 var DropdownComponent = require("substance/ui/dropdown_component");
 var Icon = require("substance/ui/font_awesome_icon");
 
-var CiteToolComponent = require("./tools/cite_tool_component");
-var NavigateTool = require("./tools/navigate_tool_component");
+var CiteTool = require("./tools/cite_tool_component");
 
 function ContentToolbarComponent() {
   Component.apply(this, arguments);
@@ -24,23 +24,23 @@ ContentToolbarComponent.Prototype = function() {
     var el = $$("div").addClass("content-tools-component toolbar small fill-white");
     el.append(
       $$('div').addClass('tool-group text clearfix').append(
-        $$(TextToolComponent).addProps({ tool: 'text', title: i18n.t('menu.text_tool')})
+        $$(TextTool).addProps({ tool: 'text', title: i18n.t('menu.text_tool')})
       )
     );
     el.append(
       $$('div').addClass('tool-group document clearfix').append(
-        $$(ToolComponent)
+        $$(Tool)
           .addClass('button tool')
           .addProps({
             tool: 'undo',
             title: i18n.t('menu.undo')
           })
           .append($$(Icon).addProps({icon: "fa-undo"})),
-        $$(ToolComponent)
+        $$(Tool)
           .addProps({ tool: 'redo', title: i18n.t('menu.redo')})
           .addClass('button tool')
           .append($$(Icon).addProps({icon: "fa-repeat"})),
-        $$(ToolComponent)
+        $$(Tool)
           .addProps({ tool: 'export', title: i18n.t('menu.export') })
           .addClass('button tool')
           .append($$(Icon).addProps({icon: "fa-download"}))
@@ -52,22 +52,18 @@ ContentToolbarComponent.Prototype = function() {
         label: $$(Icon).addProps({icon: "fa-image"}),
         title: i18n.t('figure')
       }).append(
-        $$(ToolComponent)
+        $$(Tool)
           .addProps({ tool: 'insert_figure'})
           .addClass('option')
           .append("Insert"),
-        $$(CiteToolComponent)
+        $$(CiteTool)
           .addProps({ citationType: 'image_figure'})
           .addClass('option')
           .append("Cite"),
-        $$(NavigateTool)
+        $$(OpenModalTool)
           .addProps({
-            newState: {
-              modal: {
-                contextId: 'manageCollection',
-                itemType: 'image_figure'
-              }
-            },
+            contextId: 'manageCollection',
+            itemType: 'image_figure',
             title: i18n.t('menu.manage')
           })
     );
@@ -81,42 +77,38 @@ ContentToolbarComponent.Prototype = function() {
       })
       .addClass('table-dropdown')
       .append(
-        $$(ToolComponent)
+        $$(Tool)
           .addProps({ tool: 'insert_table'})
           .addClass('option')
           .append("Insert"),
-        $$(CiteToolComponent)
+        $$(CiteTool)
           .addProps({ citationType: 'table_figure'})
           .addClass('option')
           .append("Cite"),
-        $$(NavigateTool)
+        $$(OpenModalTool)
           .addProps({
-            newState: {
-              modal: {
-                contextId: 'manageCollection',
-                itemType: 'table_figure'
-              }
-            },
+            contextId: 'manageCollection',
+            itemType: 'table_figure',
             title: i18n.t('menu.manage')
           }),
         $$("hr"),
-        $$(TableToolComponent)
+        $$(TableTool)
           .addClass('option')
           .addProps({ tool: 'insert_columns', mode: "before"}),
-        $$(TableToolComponent)
+        $$(TableTool)
           .addProps({ tool: 'delete_columns' })
           .addClass('option'),
-        $$(TableToolComponent)
+        $$(TableTool)
           .addProps({ tool: 'insert_columns', mode: "after"})
           .addClass('option'),
         $$("hr"),
-        $$(TableToolComponent)
+        $$(TableTool)
           .addProps({ tool: 'insert_rows', mode: "above"})
           .addClass('option'),
-        $$(TableToolComponent)
+        $$(TableTool)
           .addProps({ tool: 'delete_rows' })
           .addClass('option'),
-        $$(TableToolComponent)
+        $$(TableTool)
           .addProps({ tool: 'insert_rows', mode: "below"})
           .addClass('option')
       );
@@ -129,17 +121,13 @@ ContentToolbarComponent.Prototype = function() {
         title: i18n.t('bibitem')
       })
       .append(
-        $$(CiteToolComponent)
+        $$(CiteTool)
           .addProps({ citationType: 'bib_item' })
           .addClass('option')
           .append("Cite"),
-        $$(NavigateTool)
+        $$(OpenModalTool)
           .addProps({
-            newState: {
-              modal: {
-                contextId: 'manageBibItems'
-              }
-            },
+            contextId: 'manageBibItems',
             title: i18n.t('menu.manage')
           })
       );
@@ -151,11 +139,11 @@ ContentToolbarComponent.Prototype = function() {
         bibitemActions
       ),
       $$('div').addClass('tool-group formatting clearfix float-right').append(
-        $$(ToolComponent)
+        $$(Tool)
           .addClass('button tool')
           .addProps({ tool: 'emphasis', title: i18n.t('menu.emphasis')})
           .append($$(Icon).addProps({icon: "fa-italic"})),
-        $$(ToolComponent)
+        $$(Tool)
           .addClass('button tool')
           .addProps({ tool: 'strong', title: i18n.t('menu.strong') })
           .append($$(Icon).addProps({icon: "fa-bold"}))
