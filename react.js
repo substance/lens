833e06f7ba71e4c5a7816f0c55d29ef73e273ed6
Michael Aufreiter <michael@substance.io>
2015-10-05 19:18:15 +0200

    Get rid of external dependency injection requirements.
    Start implementing a React wrapper.

---
+++ react.js
@@ -0,0 +1,65 @@
+var React = require('react');
+
+// Usage:
+// var LensWriter = require('lens-writer/adapters/react');
+// 
+// React.createElement(LensWriter, {
+//   content: LENS_ARTICLE_XML,
+//   handleSave: function(doc, cb) {
+//     var xml = doc.toXml();
+//   }
+// });
+
+// Lens + Substance
+var LensWriter = require('lens-writer');
+var Article = require('lens-writer/article');
+var Component = require('substance/component');
+var $$ = Component.$$;
+
+var ReactLensWriter = React.createClass({
+
+  handleSave: function(doc, cb) {
+    this.props.handleSave(doc, cb);
+  },
+
+  componentWillUnmount: function() {
+    // this.state.writer.empty();
+  },
+
+  /*  If we get updated, we should tear down the writer and bring it up again. */
+  componentWillUpdate: function() {
+    this.componentWillUnmount();
+  },
+
+  // New props arrived, update the editor
+  componentDidUpdate: function() {
+    var doc = Article.fromXml(this.props.content);
+    this.state.writer.setProps({
+      doc: doc
+    });
+  },
+
+  componentDidMount() {
+    var el = React.findDOMNode(this);
+    var doc = Article.fromXml(this.props.content);
+
+    var writer = Component.mount($$(LensWriter, {doc: doc}), el);
+    this.setState({
+      writer: writer
+    });
+  },
+
+  render: function() {
+    return React.DOM.div({
+      className: 'lens-writer-wrapper'
+    });
+  }
+});
+
+
+ReactLensWriter.propTypes = {
+  content: React.PropTypes.string // XML source of the document
+};
+
+
+module.exports = ReactLensWriter;
