d47137670df5754d5531616f6c353bae39d09b6a
Michael Aufreiter <michael@substance.io>
2015-10-27 00:16:40 +0100

    React wrapper for LensReader.

--- ReactLensReader.js
+++ ReactLensReader.js
@@ -1,36 +1,26 @@
 var React = require('react');
-var LensWriter = require('./LensWriter');
+var LensReader = require('./LensReader');
 var Article = require('./model/LensArticle');
 var Component = require('substance/ui/Component');
 var $$ = Component.$$;
 
-// LensWriter wrapped in a React component
+// LensReader wrapped in a React component
 // ------------------
 
-var ReactLensWriter = React.createClass({
+var ReactLensReader = React.createClass({
 
-  getWriter: function() {
-    return this;
-  },
-
-  // Delegators
-  _onSave: function(doc, changes, cb) {
-    var xml = doc.toXml();
-    this.props.onSave(xml, cb);
-  },
-
-  _onFileUpload: function(file, cb) {
-    this.props.onFileUpload(file, cb);
+  getReader: function() {
+    return this.reader;
   },
 
   getContent: function() {
-    return this.writer.getDocument().toXml();
+    return this.reader.getDocument().toXml();
   },
 
   // New props arrived, update the editor
   componentDidUpdate: function() {
     var doc = this.createDoc(this.props.content);
-    this.writer.extendProps({
+    this.reader.extendProps({
       doc: doc
     });
   },
@@ -42,22 +32,20 @@ var ReactLensWriter = React.createClass({
   componentDidMount: function() {
     var el = React.findDOMNode(this);
     var doc = this.createDoc(this.props.content);
-    this.writer = Component.mount($$(LensWriter, {
-      doc: doc,
-      onSave: this._onSave,
-      onFileUpload: this._onFileUpload
+    this.reader = Component.mount($$(LensReader, {
+      doc: doc
     }), el);
   },
 
   render: function() {
     return React.DOM.div({
-      className: 'lens-writer-wrapper'
+      className: 'lens-reader-wrapper'
     });
   }
 });
 
-ReactLensWriter.propTypes = {
+ReactLensReader.propTypes = {
   content: React.PropTypes.string // XML source of the document
 };
 
-module.exports = ReactLensWriter;
+module.exports = ReactLensReader;
