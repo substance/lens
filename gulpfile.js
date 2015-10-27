ec66e7dabce3475532f4cb0b36ed0aa32beb2d4f
Michael Aufreiter <michael@substance.io>
2015-10-27 13:55:32 +0100

    Adjust gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserify = require('browserify');
-var babelify = require('babelify');
 var uglify = require('gulp-uglify');
 var through2 = require('through2');
-var path = require('path');
 
 gulp.task('sass', function () {
   gulp.src('./app/app.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./dist'));
 });
-
 
 gulp.task('assets', function () {
   gulp.src('./app/assets/**/*', {base:"./app/assets"})
@@ -25,11 +22,10 @@ gulp.task('data', function () {
     .pipe(gulp.dest('./dist/data'));
 });
 
-gulp.task('babel', function () {
+gulp.task('bundle', function () {
     return gulp.src('./app/app.js')
         .pipe(through2.obj(function (file, enc, next) {
             browserify(file.path)
-                .transform(babelify.configure({ only: [ path.join(__dirname, 'src') ] }))
                 .bundle(function (err, res) {
                     if (err) { return next(err); }
                     file.contents = res;
@@ -44,4 +40,4 @@ gulp.task('babel', function () {
         .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('default', ['assets', 'data', 'sass', 'babel']);
+gulp.task('default', ['assets', 'data', 'sass', 'bundle']);
