cd316d60d07b02dca5d96a97d890bd8f8b5c68df
Michael Aufreiter <michael@substance.io>
2015-10-27 13:06:05 +0100

    CitationCommand requires its own implementation
    - Inserts fake character
    - Attaches citation annotation to it

--- packages/citations/CitationCommand.js
+++ packages/citations/CitationCommand.js
@@ -1,23 +1,81 @@
 'use strict';
 
-var AnnotationCommand = require('substance/ui/AnnotationCommand');
+var SurfaceCommand = require('substance/ui/SurfaceCommand');
+var createAnnotation = require('substance/model/transform/createAnnotation');
+var insertText = require('substance/model/transform/insertText');
 
-var ToggleBibItemCitationCommand = AnnotationCommand.extend({
+var CitationCommand = SurfaceCommand.extend({
 
-  // When there's no existing annotation overlapping, we create a new one.
-  canCreate: function(annos) {
-    return annos.length === 0;
-  },
+  getCommandState: function() {
+    var sel = this.getSelection();
+    var newState = {
+      disabled: true,
+      active: false
+    };
 
-  canDelete: function() {
-    return false;
+    if (sel && !sel.isNull() && sel.isPropertySelection() && sel.isCollapsed()) {
+      newState.disabled = false;
+    }
+    return newState;
   },
 
   getAnnotationData: function() {
     return {
-      target: []
+      targets: []
+    };
+  },
+
+  getAnnotationType: function() {
+    if (this.constructor.static.annotationType) {
+      return this.constructor.static.annotationType;
+    } else {
+      throw new Error('Contract: CitationCommand.static.annotationType should be associated to a document citation type.');
+    }
+  },
+
+  execute: function() {
+    var state = this.getCommandState();
+    var doc = this.getDocument();
+    var surface = this.getSurface();
+    var newAnno;
+    
+    // Return if command is disabled
+    if (state.disabled) return;
+
+    surface.transaction(function(tx, args) {
+      var collapsedSel = surface.getSelection();
+
+      // 1. Insert fake character where the citation should stick on
+      args = insertText(tx, {
+        selection: collapsedSel,
+        text: '$'
+      });
+
+      var citationSel = doc.createSelection({
+        type: 'property',
+        path: collapsedSel.path,
+        startOffset: collapsedSel.startOffset-1,
+        endOffset: collapsedSel.endOffset
+      });
+
+      // 2. Create citation annotation
+      args.annotationType = this.getAnnotationType();
+      args.annotationData = this.getAnnotationData();
+      args.selection = citationSel;
+      args.splitContainerSelections = false;
+      args.containerId = surface.getContainerId();
+
+      args = createAnnotation(tx, args);
+      newAnno = args.result;
+      return args;
+    }.bind(this));
+
+    return {
+      mode: 'create',
+      anno: newAnno
     };
   }
+
 });
 
-module.exports = ToggleBibItemCitationCommand;
+module.exports = CitationCommand;
