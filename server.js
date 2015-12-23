0d6f1290d704d31ca996aff5963426ea65102e49
Michael Aufreiter <michael@substance.io>
2015-12-23 12:43:13 +0100

    Font-awesome via npm. Cleanup of server.js.

--- server.js
+++ server.js
@@ -1,68 +1,24 @@
-var http = require('http');
 var express = require('express');
 var path = require('path');
-var sass = require('node-sass');
+var server = require('substance/util/server');
 var bodyParser = require('body-parser');
 
 var app = express();
 var port = process.env.PORT || 5000;
-var browserify = require("browserify");
 
 // use body parser so we can get info from POST and/or URL parameters
 app.use(bodyParser.json({limit: '3mb'}));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // use static server
+app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "app/assets")));
 app.use(express.static(path.join(__dirname, "app/data")));
 app.use('/i18n', express.static(path.join(__dirname, "app/i18n")));
+app.use('/fonts', express.static(path.join(__dirname, 'node_modules/font-awesome/fonts')));
 
-// Backend
-// --------------------
-
-app.get('/app.js', function (req, res, next) {
-  // var startTime = Date.now();
-  browserify({ debug: true, cache: false })
-    .add(path.join(__dirname, "app", "app.js"))
-    .bundle()
-    .on('error', function(err){
-      console.error(err.message);
-      res.send('console.log("'+err.message+'");');
-    })
-    .pipe(res);
-});
-
-var handleError = function(err, res) {
-  console.error(err);
-  res.status(400).json(err);
-};
-
-var renderSass = function(cb) {
-  sass.render({
-    file: path.join(__dirname, "app", "app.scss"),
-    sourceMap: true,
-    outFile: 'app.css',
-  }, cb);
-};
-
-// use static server
-app.use(express.static(__dirname));
-
-app.get('/app.css', function(req, res) {
-  renderSass(function(err, result) {
-    if (err) return handleError(err, res);
-    res.set('Content-Type', 'text/css');
-    res.send(result.css);
-  });
-});
-
-app.get('/app.css.map', function(req, res) {
-  renderSass(function(err, result) {
-    if (err) return handleError(err, res);
-    res.set('Content-Type', 'text/css');
-    res.send(result.map);
-  });
-});
+server.serveStyles(app, '/app.css', path.join(__dirname, 'app', 'app.scss'));
+server.serveJS(app, '/app.js', path.join(__dirname, 'app', 'app.js'));
 
 app.listen(port, function(){
   console.log("Lens running on port " + port);
